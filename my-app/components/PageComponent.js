ab76006e40678a21670ac8fe4c7a965e6aa7b909
Geir Huus Skeie <geir.huus.skeie@gmail.com>
2024-02-08 12:08:32 +0100

    Added Api call for muscle group name and instructions

--- my-app/components/PageComponent.js
+++ my-app/components/PageComponent.js
@@ -1,75 +1,108 @@
-import {Text, View, TouchableOpacity, ScrollView} from "react-native"
+import { Text, View, TouchableOpacity, ScrollView } from "react-native";
 import { useState } from "react";
-import DataTableComponent from "./DataTableComponent"
-import SearchBar from "./SearchBar"
+import DataTableComponent from "./DataTableComponent";
+import SearchBar from "./SearchBar";
 
+const PageComponent = ({ exerciseList, muscle, favorites }) => {
+  const [showMostPopular, setShowMostPopular] = useState(false);
 
-
-
-const PageComponent = ({ exerciseList, allExercises, favorites }) => {
-    const [showMostPopular, setShowMostPopular] = useState(false);
-
-
-    return (
+  return (
     <ScrollView>
-    <View style={{ flex:1, alignItems:'center'}}>
-    <View style={{ width:'80%' }}>
-        <View style={{flexDirection:'row', justifyContent:'space-evenly', marginTop:30}}>
-                <TouchableOpacity onPress ={() => setShowMostPopular(true)}>
-                {showMostPopular ? (
-                    <Text style={{ fontSize:16, fontWeight: 'bold' }}>Most popular</Text>
-                ) : <Text style={{ fontSize:16 }}>Most popular</Text>
-                }
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <View style={{ width: "80%" }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              marginTop: 30,
+            }}
+          >
+            <TouchableOpacity onPress={() => setShowMostPopular(true)}>
+              {showMostPopular ? (
+                <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                  Most popular
+                </Text>
+              ) : (
+                <Text style={{ fontSize: 16 }}>Most popular</Text>
+              )}
+            </TouchableOpacity>
 
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={() => setShowMostPopular(false)}>
-                {!showMostPopular ? (
-                     <Text style={{ fontSize:16, fontWeight: 'bold'}}>Favorites</Text>
-                ) :  <Text style={{ fontSize:16 }}>Favorites</Text>
-                }
-                </TouchableOpacity>
-
-            </View>
-            <View style={{
-            elevation: 4,
-                   padding:10, marginTop:10, width: '100%', backgroundColor:'#C6D9BA', borderRadius:10}}>
+            <TouchableOpacity onPress={() => setShowMostPopular(false)}>
+              {!showMostPopular ? (
+                <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                  Favorites
+                </Text>
+              ) : (
+                <Text style={{ fontSize: 16 }}>Favorites</Text>
+              )}
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              elevation: 4,
+              padding: 10,
+              marginTop: 10,
+              width: "100%",
+              backgroundColor: "#C6D9BA",
+              borderRadius: 10,
+            }}
+          >
             {showMostPopular ? (
-            <View>
-             <View style={{  justifyContent: 'center', alignItems: 'center' }}>
-               <Text style={{ fontSize:20, marginBottom:5, textDecorationLine:'underline'}}>Most popular</Text>
-             </View>
-             {exerciseList.map((item) => (
-               <View key={item.id}>
-                 <Text style={{ fontSize: 20 }}>{item.id}. {item.title}</Text>
-               </View>
-             ))}
-           </View>
-
+              <View>
+                <View
+                  style={{ justifyContent: "center", alignItems: "center" }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      marginBottom: 5,
+                      textDecorationLine: "underline",
+                    }}
+                  >
+                    Most popular
+                  </Text>
+                </View>
+                {exerciseList.map((item) => (
+                  <View key={item.id}>
+                    <Text style={{ fontSize: 20 }}>
+                      {item.id}. {item.title}
+                    </Text>
+                  </View>
+                ))}
+              </View>
             ) : (
               <View>
-              <View style={{  justifyContent: 'center', alignItems: 'center' }}>
-                 <Text style={{ fontSize:20, marginBottom:5, textDecorationLine:'underline' }}>Favorites</Text>
-               </View>
-                {favorites.map(item => (
+                <View
+                  style={{ justifyContent: "center", alignItems: "center" }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      marginBottom: 5,
+                      textDecorationLine: "underline",
+                    }}
+                  >
+                    Favorites
+                  </Text>
+                </View>
+                {favorites.map((item) => (
                   <View key={item.id}>
-                    <Text style={{ fontSize:20 }}>{item.id}. {item.title}</Text>
+                    <Text style={{ fontSize: 20 }}>
+                      {item.id}. {item.title}
+                    </Text>
                   </View>
                 ))}
               </View>
             )}
-
-
-            </View>
-            <View style={{ marginTop:30 }}>
-                    <SearchBar />
-                    </View>
-        <DataTableComponent allExercises = {allExercises}/>
+          </View>
+          <View style={{ marginTop: 30 }}>
+            <SearchBar />
+          </View>
+          <DataTableComponent muscle={muscle} />
         </View>
-
-    </View>
+      </View>
     </ScrollView>
-    )
-}
+  );
+};
 
 export default PageComponent;
--- my-app/pages/Chest.js
+++ my-app/pages/Chest.js
@@ -1,30 +1,23 @@
-import {Text, View, ScrollView} from "react-native"
-import SearchBar from "../components/SearchBar"
 import PageComponent from "../components/PageComponent";
-import { DataTable } from "react-native-paper";
 import { useState } from "react";
-
-
-
-
-
+import ApiNinja from "../components/Api-Calls/ApiNinja";
 
 const Chest = () => {
-    const [benchExercises] = useState([
-        { id: '1', title: 'Bench press'},
-        { id: '2', title: 'Push ups'},
-        { id: '3', title: 'Incline Bench press'},
-        { id: '4', title: 'Machine Flies'},
+  const [benchExercises] = useState([
+    { id: "1", title: "Bench press" },
+    { id: "2", title: "Push ups" },
+    { id: "3", title: "Incline Bench press" },
+    { id: "4", title: "Machine Flies" },
+  ]);
 
-    ]);
-
-    const [favoritesChestExercises] = useState(
-    [
-        {}
-    ]    );
-    return (
-        <PageComponent exerciseList={benchExercises} allExercises={benchExercises} favorites = {favoritesChestExercises}/>
-    )
-}
+  const [favoritesChestExercises] = useState([{}]);
+  return (
+    <PageComponent
+      exerciseList={benchExercises}
+      muscle={"chest"}
+      favorites={favoritesChestExercises}
+    />
+  );
+};
 
 export default Chest;
